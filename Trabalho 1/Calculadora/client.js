77b81c7fa4cb89961f539ee6719b9e1519de09cb
Lucas Noronha <lucasnoronha7777@gmail.com>
2022-09-18 21:59:50 -0300

    Melhorando padrão das mensagens e tratando erros

--- Trabalho 1/Calculadora/client.js
+++ Trabalho 1/Calculadora/client.js
@@ -1,36 +1,72 @@
 const net = require('net');
-const readline = require('readline');
+const read = require('readline-sync');
 
 const port = 8080;
 const address = 'localhost';
 
 const client = new net.Socket();
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+const firstValue = read.question('Type the first value: ');
+const secondValue = read.question('Type the second value: ');
+const operation = read.question('Type the operation: [+, -, *, /] ');
+const type = read.question('Integer [i] or float [f] operation? ');
+
+const message = buildMessage(firstValue, secondValue, operation, type);
+
+client.connect(port, address, () => {
+    console.log('Sending data...');
+    client.write(message)
+})
+
+client.on('data', (stream) => {
+
+    const result = stream.toString().split(' ');
+    const status = result[0];
+    result.shift();
+    const message = result.join(' ');
+
+    if (status == 'SUCCESS'){
+        console.log(`Answer: ${message}`);
+    }
+
+    else if (status == 'ERROR') {
+        console.log(`Error from server: ${message}`);
+    }
+
+    client.destroy();
+    console.log('Connection closed');
 });
 
-rl.question('Digite o primeiro valor: ', (firstValue) => {
-    rl.question('Digite o segundo valor: ', (secondValue) => {
-        rl.question('Digite a operação: ', (operation) => {
-            
-            client.connect(port, address, () => {
-                console.log('Sending data...');
-                client.write(`${firstValue} ${operation} ${secondValue}`);
-            });
+function buildMessage(firstValue, secondValue, operation, type) {
+    let formattedOperation;
 
-            client.on('data', (stream) => {
-                console.log(`Answer: ${stream}`);
-            });
+    if (operation == '+') formattedOperation = 'SUM';
+    else if (operation == '-') formattedOperation = 'SUB';
+    else if (operation == '*') formattedOperation = 'MULT';
+    else if (operation == '/') formattedOperation = 'DIV';
+    else {
+        console.log(`Error from client: invalid operation ${operation}`);
+        process.exit();
+    }
 
-            client.on('close', () => console.log('Connection closed'));
-            rl.close();
-        });
-    });
-});
+    let formattedType;
 
+    if (type == 'i') formattedType = 'INTEGER';
+    else if (type == 'f') formattedType = 'FLOAT';
+    else {
+        console.log(`Error from client: invalid type ${type}`);
+        process.exit();
+    }
 
+    if (isNaN(firstValue)) {
+        console.log(`Error from client: first value is not a number`);
+        process.exit();
+    }
 
+    if (isNaN(secondValue)) {
+        console.log(`Error from client: second value is not a number`);
+        process.exit();
+    }
 
-
+    return `${firstValue} ${secondValue} ${formattedOperation} ${formattedType}`;
+}
--- Trabalho 1/Calculadora/server.js
+++ Trabalho 1/Calculadora/server.js
@@ -1,30 +1,64 @@
 const net = require('net');
+const { TLSSocket } = require('tls');
 
 const port = 8080;
 const address = 'localhost';
 
 const server = net.createServer();
 
+function writeError(socket, message = 'An error occurred') {
+    socket.write(`ERROR ${message}`);
+    console.log(`Error from: ${socket.address().address}`);
+    
+    throw new Error(message);
+}
+
 server.on('connection', (socket) => {
     socket.on('data', (stream) => {
         
         const symbols = stream.toString().split(' ');
-        const firstValue = parseInt(symbols[0]);
-        const secondValue = parseInt(symbols[2]);
-        const operation = symbols[1];
 
+        if (symbols.length != 4) writeError(socket, 'invalid message format');
+
+        const valueType = symbols[3];
+
+        let firstValue, secondValue;
+
+        if (valueType == 'FLOAT') {
+            firstValue = parseFloat(symbols[0]);
+            secondValue = parseFloat(symbols[1]);
+        }
+        else if (valueType == 'INTEGER') {
+            firstValue = parseInt(symbols[0]);
+            secondValue = parseInt(symbols[1]);
+        }
+
+        else writeError(socket, 'missing or invalid value type');
+
+        const operation = symbols[2];
         let result;
 
-        if (operation == '+') result = firstValue + secondValue;
-        else if (operation == '-') result = firstValue - secondValue;
-        else if (operation == '*') resutl = firstValue * secondValue;
-        else if (operation == '/') result = firstValue / secondValue;
+        if (operation == 'SUM') result = firstValue + secondValue;
+        else if (operation == 'SUB') result = firstValue - secondValue;
+        else if (operation == 'MULT') result = firstValue * secondValue;
+        else if (operation == 'DIV') result = firstValue / secondValue;
+        else {
+            writeError(socket, `invalid operation ${operation}`);
+        }
 
-        socket.write(result.toString());
+        if (valueType == 'INTEGER') result = parseInt(result);
+
+        socket.write(`SUCCESS ${result.toString()}`);
         socket.destroy();
     });
+
+    socket.on('error', (error) => console.log(error));
 });
 
 server.listen(port, address, () => {
     console.log(`Listening on port ${port}...`);
+});
+
+process.on('uncaughtException', (error) => {
+    console.log(`message: ${error.message}`);
 });
