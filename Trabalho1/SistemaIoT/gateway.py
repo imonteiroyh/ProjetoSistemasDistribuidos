5d762302cf50982699bdc48dd4437256f153aba3
Lucas Noronha <lucasnoronha7777@gmail.com>
2022-09-28 19:11:14 -0300

    Adicionando threads para descoberta de sensores e tratamento de conexões

--- Trabalho1/SistemaIoT/gateway.py
+++ Trabalho1/SistemaIoT/gateway.py
@@ -1,9 +1,58 @@
 import socket
 from config import GROUP_PORT, GROUP_HOST
 from serializers import message_pb2 as proto
+import threading
+from time import sleep
 
 HOST = 'localhost'
 PORT = 7897
+
+sensors_count = 0
+
+sensors = {
+    id: ([], '')
+}
+
+def findDevices(group_socket, time=5):
+    while True:
+        print('Descobrindo dispositivos...')
+        message = proto.Message()
+        message.type = 'DISCOVER'
+        message.discover.CopyFrom(proto.Discover())
+        message.discover.ip = HOST
+        message.discover.port = PORT
+        group_socket.sendto(message.SerializeToString(), (GROUP_HOST, GROUP_PORT))
+        sleep(time)
+
+def handleConnection(socket, address, mutex):
+    global sensors_count
+    print(f'Conexão aberta: {address}')
+
+    data = socket.recv(1024)
+
+    sensor_response = proto.Message()
+    sensor_response.ParseFromString(data)
+
+    with mutex:
+        sensors_count += 1
+        sensor_id = sensors_count
+        sensors[sensor_id] = [[], sensor_response.discover.device_type]
+
+    print(f'Sensor adicionado: {sensor_response.discover.device_type}') 
+    
+    while True:
+        f = open("log.txt", 'a')
+        data = socket.recv(1024)
+        sensor_message = proto.Message()
+        sensor_message.ParseFromString(data)
+
+        if sensor_message.type == 'DATA':
+            with mutex:
+                actual_sensor_state = sensors[sensor_id]
+                actual_sensor_state[0] = [sensor_message.data.data]
+                f.write(str(sensor_id) + ': ' + str(sensors[sensor_id][0][0]) + '\n')
+        f.close()
+
 
 print('Iniciando servidor...')
 group_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -17,21 +66,13 @@ server_socket.bind((HOST, PORT))
 server_socket.listen()
 print(f'Servidor ouvindo no endereço {HOST}:{PORT}')
 
-message = proto.Message()
-message.type = 'DISCOVER'
-message.discover.CopyFrom(proto.Discover())
-message.discover.ip = HOST
-message.discover.port = PORT
 
-print('Descobrindo dispositivos...')
-group_socket.sendto(message.SerializeToString(), (GROUP_HOST, GROUP_PORT))
+discover_devices_thread = threading.Thread(target=findDevices, args=(group_socket, ))
+discover_devices_thread.start()
 
-connection_socket, address = server_socket.accept()
-print(f'Conexão aberta: {address}')
+mutex = threading.Lock()
 
-data = connection_socket.recv(1024)
-
-sensor_response = proto.Message()
-sensor_response.ParseFromString(data)
-
-print(f'Sensor adicionado: {sensor_response.discover.device_type}')
+while True:
+    connection_socket, address = server_socket.accept()
+    connection_thread = threading.Thread(target=handleConnection, args=(connection_socket, address, mutex))
+    connection_thread.start()
--- Trabalho1/SistemaIoT/temperature_sensor.py
+++ Trabalho1/SistemaIoT/temperature_sensor.py
@@ -3,14 +3,15 @@ from time import sleep
 import struct
 from config import GROUP_HOST, GROUP_PORT
 from serializers import message_pb2 as proto
+from random import randint
 
 HOST = 'localhost'
-PORT = 7899
+PORT = 7900
 
 print('Iniciando sensor...')
 group_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-group_socket.bind((GROUP_HOST, GROUP_PORT))
 group_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+group_socket.bind((GROUP_HOST, GROUP_PORT))
 mreq = struct.pack("4sl", socket.inet_aton(GROUP_HOST), socket.INADDR_ANY)
 group_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 print(f'Comunicação em grupo no endereço {GROUP_HOST}:{GROUP_PORT}')
@@ -27,8 +28,6 @@ if gateway_discover.type == 'DISCOVER':
 
 print(f'Gateway identificado: {discover_message.ip}:{discover_message.port}')
 
-sleep(0.5)
-
 print('Conectando ao gateway...')
 response_discover = proto.Discover()
 response_discover.device_type = 'TEMPERATURE_SENSOR'
@@ -41,3 +40,15 @@ response.discover.CopyFrom(response_discover)
 
 sensor_socket.connect((discover_message.ip, discover_message.port))
 sensor_socket.send(response.SerializeToString())
+
+while True:
+    sleep(1)
+
+    data = proto.Data()
+    data.data = str(randint(10, 30))
+
+    data_message = proto.Message()
+    data_message.type = 'DATA'
+    data_message.data.CopyFrom(data)
+
+    sensor_socket.send(data_message.SerializeToString())
