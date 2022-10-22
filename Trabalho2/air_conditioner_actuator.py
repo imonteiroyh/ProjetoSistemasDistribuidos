cdecad5a8ad07deadb53c97ae66f1b897630ee8e
Lucas Noronha <lucasnoronha7777@gmail.com>
2022-10-22 15:22:13 -0300

    Adicionando docker composer do RabbitMQ e Configurando fila do sensor de temperatura

--- Trabalho2/air_conditioner_actuator.py
+++ Trabalho2/air_conditioner_actuator.py
@@ -1,8 +1,14 @@
-from config import HOST, AIR_CONDITIONER_PORT
+from ast import arg
 import grpc
 from concurrent import futures
+import threading
 from proto import air_conditioner_pb2
 from proto import air_conditioner_pb2_grpc
+from config import HOST, AIR_CONDITIONER_PORT
+from random import randint
+import pika
+from time import sleep
+
 
 class AirConditionerService(air_conditioner_pb2_grpc.AirConditionerServicer):
 
@@ -13,13 +19,44 @@ class AirConditionerService(air_conditioner_pb2_grpc.AirConditionerServicer):
     def get_temperature(self, request, context):
         return air_conditioner_pb2.AirConditionerResponse(status = True, message = 'Temperature is 25')
 
+
+class TemperatureSensor:
+
+    def __init__(self, RABBITMQ_HOST):
+        self.exchange_name = 'temperature_sensor'
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST))
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type='fanout')
+        print('Connected to RabbitMq')
+
+    def generate_data(self):
+        print('Generating data...')
+        while True:
+            temperature = randint(15, 30)
+            self.channel.basic_publish(
+                exchange=self.exchange_name,
+                routing_key='',
+                body=str(temperature)
+            )
+            sleep(5)
+
+    def run(self):
+        t = threading.Thread(target=self.generate_data)
+        t.start()
+       
+
+
 def main():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers = 10))
-    air_conditioner_pb2_grpc.add_AirConditionerServicer_to_server(AirConditionerService(), server)
-    server.add_insecure_port(HOST + ':' + AIR_CONDITIONER_PORT)
-    server.start()
-    print('Funcionando')
-    server.wait_for_termination()
+
+    temperature_sensor = TemperatureSensor(HOST)
+    temperature_sensor.run()
+
+    # server = grpc.server(futures.ThreadPoolExecutor(max_workers = 10))
+    # air_conditioner_pb2_grpc.add_AirConditionerServicer_to_server(AirConditionerService(), server)
+    # server.add_insecure_port(HOST + ':' + AIR_CONDITIONER_PORT)
+    # server.start()
+    # print('Funcionando')
+    # server.wait_for_termination()
 
 if __name__ == '__main__':
     try:
--- Trabalho2/home_assistant.py
+++ Trabalho2/home_assistant.py
@@ -6,22 +6,51 @@ from proto import air_conditioner_pb2
 from proto import air_conditioner_pb2_grpc
 from proto import blind_curtain_pb2
 from proto import blind_curtain_pb2_grpc
+import pika
+import threading
 
 def main():
-    with grpc.insecure_channel(HOST + ':' + LAMP_PORT) as LampChannel:
-        lamp_service = lamp_pb2_grpc.LampStub(LampChannel)
-        lamp_state = lamp_service.get_state(lamp_pb2.GetLampStateRequest())
-        print(f'Lamp state: {lamp_state}')
 
-    with grpc.insecure_channel(HOST + ':' + AIR_CONDITIONER_PORT) as AirConditionerChannel:
-        air_conditioner_service = air_conditioner_pb2_grpc.AirConditionerStub(AirConditionerChannel)
-        air_conditioner_temperature = air_conditioner_service.get_temperature(air_conditioner_pb2.GetAirConditionerTemperatureRequest())
-        print(f'Air_conditioner temperature: {air_conditioner_temperature}')
+    def temperature_sensor_callback(ch, method, properties, body):
+        print(f'Data received from temperature sensor: {int(body)}')
 
-    with grpc.insecure_channel(HOST + ':' + BLIND_CURTAIN_PORT) as BlindCurtainChannel:
-        blind_curtain_service = blind_curtain_pb2_grpc.BlindCurtainStub(BlindCurtainChannel)
-        blind_curtain_state = blind_curtain_service.get_state(blind_curtain_pb2.GetBlindCurtainStateRequest())
-        print(f'Blind Curtain state: {blind_curtain_state}')
+    connection = pika.BlockingConnection(pika.ConnectionParameters(HOST))
+    channel = connection.channel()
+    print('Connected to RabbitMQ')
+
+    temperature_sensor_thread = threading.Thread(target=subscribe, args=(channel, 'temperature_sensor', temperature_sensor_callback))
+    temperature_sensor_thread.start()
+
+    # with grpc.insecure_channel(HOST + ':' + LAMP_PORT) as LampChannel:
+    #     lamp_service = lamp_pb2_grpc.LampStub(LampChannel)
+    #     lamp_state = lamp_service.get_state(lamp_pb2.GetLampStateRequest())
+    #     print(f'Lamp state: {lamp_state}')
+
+    # with grpc.insecure_channel(HOST + ':' + AIR_CONDITIONER_PORT) as AirConditionerChannel:
+    #     air_conditioner_service = air_conditioner_pb2_grpc.AirConditionerStub(AirConditionerChannel)
+    #     air_conditioner_temperature = air_conditioner_service.get_temperature(air_conditioner_pb2.GetAirConditionerTemperatureRequest())
+    #     print(f'Air_conditioner temperature: {air_conditioner_temperature}')
+
+    # with grpc.insecure_channel(HOST + ':' + BLIND_CURTAIN_PORT) as BlindCurtainChannel:
+    #     blind_curtain_service = blind_curtain_pb2_grpc.BlindCurtainStub(BlindCurtainChannel)
+    #     blind_curtain_state = blind_curtain_service.get_state(blind_curtain_pb2.GetBlindCurtainStateRequest())
+    #     print(f'Blind Curtain state: {blind_curtain_state}')
+
+def subscribe(channel, exchange, callback):
+    channel.exchange_declare(exchange=exchange, exchange_type='fanout')
+    air_conditioner_queue = channel.queue_declare(queue='', exclusive=True)
+    air_conditioner_queue_name = air_conditioner_queue.method.queue
+    channel.queue_bind(exchange=exchange, queue=air_conditioner_queue_name)
+
+    print('Waiting for data...')
+
+    channel.basic_consume(
+        queue=air_conditioner_queue_name,
+        on_message_callback=callback,
+        auto_ack=True
+    )
+
+    channel.start_consuming()
 
 if __name__ == '__main__':
     try:
