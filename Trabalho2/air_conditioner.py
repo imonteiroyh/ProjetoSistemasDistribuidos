c358f25add860df716d17fa34ecef093bc2eee52
Lucas Noronha <lucasnoronha7777@gmail.com>
2022-11-02 16:29:06 -0300

    Refatorando projeto

--- Trabalho2/air_conditioner.py
+++ Trabalho2/air_conditioner.py
@@ -1,46 +1,19 @@
-from ast import arg
 import grpc
 from concurrent import futures
-import threading
-from proto import air_conditioner_pb2
-from proto import air_conditioner_pb2_grpc
+from proto.air_conditioner_pb2_grpc import add_AirConditionerServicer_to_server
 from config import HOST, AIR_CONDITIONER_PORT
-from random import randint
-import pika
-from time import sleep
-from temperature_sensor import TemperatureSensor
+from components.temperature_sensor import TemperatureSensor
+from components.air_conditioner_acuator import AirConditionerActuator
 
 
-class AirConditionerService(air_conditioner_pb2_grpc.AirConditionerServicer):
+temperature_sensor = TemperatureSensor(HOST)
+temperature_sensor.run()
 
-    def __init__(self, change_temperature_callback) -> None:
-        self.state = True
-        self.temperature = 25
-        self.callback = change_temperature_callback
-        super().__init__()
+server = grpc.server(futures.ThreadPoolExecutor(max_workers = 10))
+air_conditioner_actuator = AirConditionerActuator(temperature_sensor.change_target)
 
-    def get_temperature(self, request, context):
-        return air_conditioner_pb2.AirConditionerResponse(status = True, message = f'{self.temperature}')
+add_AirConditionerServicer_to_server(air_conditioner_actuator, server)
 
-    def change_temperature(self, request, context):
-        self.temperature = int(request.temperature)
-        self.callback(self.temperature)
-        return air_conditioner_pb2.AirConditionerResponse(status=True, message=f'Temperature setted to {self.temperature}')
-
-def main():
-
-    temperature_sensor = TemperatureSensor(HOST)
-    temperature_sensor.run()
-
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers = 10))
-    air_conditioner_pb2_grpc.add_AirConditionerServicer_to_server(AirConditionerService(temperature_sensor.change_target), server)
-    server.add_insecure_port(HOST + ':' + AIR_CONDITIONER_PORT)
-    server.start()
-    print('Air Conditioner turned on...')
-    server.wait_for_termination()
-
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        exit(0)
+server.add_insecure_port(HOST + ':' + AIR_CONDITIONER_PORT)
+server.start()
+server.wait_for_termination()
---
+++ Trabalho2/components/air_conditioner_acuator.py
@@ -0,0 +1,20 @@
+from proto.air_conditioner_pb2 import AirConditionerResponse
+from proto.air_conditioner_pb2_grpc import AirConditionerServicer
+
+
+class AirConditionerActuator(AirConditionerServicer):
+    def __init__(self, change_temperature_callback) -> None:
+        self.state = True
+        self.temperature = 20
+        self.callback = change_temperature_callback
+        self.callback(self.temperature)
+        super().__init__()
+
+    def get_temperature(self, request, context):
+        return AirConditionerResponse(status=True, message = f'{self.temperature}')
+
+    def change_temperature(self, request, context):
+        self.temperature = int(request.temperature)
+        self.callback(self.temperature)
+        return AirConditionerResponse(status=True, message=f'Temperature setted to {self.temperature}')
+
